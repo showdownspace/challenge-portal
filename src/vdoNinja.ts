1373b9c97cfa4fe605787c1ad366e5c55a787600
dtinth on MBP M1 <dtinth@spacet.me>
2024-11-19 22:39:56 +0700

    update room

--- src/vdoNinja.ts
+++ src/vdoNinja.ts
@@ -1,10 +1,13 @@
-const roomName = "showdown_space";
+const roomName = "showdown_space_room";
 const password = "8fdc0f87560fac56dea937b6839d4de517036443c2a5d5e7";
+const codirector = "d808aa3f9a20c664d3ea42a41bee708f11b8032064dfbe31";
 
 export function getDirectorUrl() {
   return `https://vdo.ninja/?${new URLSearchParams({
     director: roomName,
     password,
+    codirector,
+    hidedirector: "",
   })}`;
 }
 
